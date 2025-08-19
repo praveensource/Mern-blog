4f4e08eb311fff53e3a1b665eb6a24be55ffce41
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-08-20 00:31:14 +0530

    create header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,17 @@
+import flowbiteReact from "flowbite-react/plugin/tailwindcss";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    ".flowbite-react\\class-list.json",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "node_modules/flowbite/**/*.js",
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbiteReact],
 }
