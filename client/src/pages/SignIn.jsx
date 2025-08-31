270b89060866163717cc5f2040a3f250fb2528b5
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-08-31 23:33:17 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,17 @@
 import { Alert, Button, Spinner } from "flowbite-react";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { data, Link, useNavigate } from "react-router-dom";
+import { signInstart, signInFailure, signInSuccess } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const SignIn = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {loading, error:errorMessage} = useSelector(state => state.user);
+  
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -15,11 +20,10 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if( !formData.email || !formData.password){
-      return setErrorMessage("Please fill out all fields.")
+      return dispatch(signInFailure('please fill out all fields!!'))
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInstart);
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -27,15 +31,15 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false){
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+      
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message))
     }
   };
 
